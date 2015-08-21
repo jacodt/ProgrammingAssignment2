8b8da7c61f301244a17cae6f148ef44a50136c20
jacodt <jacodt@gmail.com>
2015-08-21 21:22:11 +0200

    Update skeleton function cachematrix.R
    Creates the two functions required for the cachematrix to operate.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a matrix object that allows for the caching of the inverse
 
-## Write a short comment describing this function
+
+## Initial constructor function for the cache matrix, takes a normal matrix as argument
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Function accessing a cache matrix object that returns the either the inverse of the 
+## matrix object or the cached version thereof
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
